4a66a1d1ae2f1601380be7e649e70e216bddb6dc
dawidplesniarski <dawidplesniarski@gmail.com>
2020-09-01 18:06:59 +0200

    Users entity splitted to UsersController.js and 'users' routes

--- controller/UsersController.js
+++ controller/UsersController.js
@@ -1,17 +1,122 @@
 const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv/config');
-app.use(bodyParser.json());
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const usersRoute = require('./routes/users');
 
-app.use('/users',usersRoute);
+const UserController = {
+  findByID: async (req,res)=>{
+      try{
+          const user = await User.findById(req.params.userId);
+          res.json(user)
+      } catch(err){
+          res.json({message:err})
+      }
+  },
+  findAll: async(req, res) =>{
+     try{
+         const users = await User.find();
+         res.json(users);
+     } catch(err){
+         res.json({message: err})
+     }
+  },
+  login: async (req, res)=>{
+      try{
+          const user = await User.findOne({login : req.body.login});
 
-const url = 'mongodb+srv://dawid:dawid@cluster0-qrt6h.mongodb.net/test?retryWrites=true&w=majority';
+          const validPassword = await bcrypt.compare(
+              req.body.password,
+              user.password
+          );
 
-mongoose.connect(url,
-    {useNewUrlParser: true},
-    ()=> console.log('DB connected')
-);
+          if(validPassword){
+              jwt.sign({user},'secretkey',(err,token)=>{
+                  res.json({
+                      user: user,
+                      token: token
+                  });
+              })
+          }else{
+              throw new Error('Login or password wrong');
+          }
+      } catch (error){
+          res.status(403);
+          res.json({message:error});
+      }
+  },
+    deleteUserByID: async(req, res) =>{
+        User.findByIdAndDelete(req.params.userId, (err) => {
+            if (err) {
+                res.status(404).send({message: `User with id: ${req.params.userId} not exists`});
+            } else {
+                res.send({message:`User with id: ${req.params.userId} deleted successfully`});
+            }
+        });
+    },
+    addUser: async (req,res)=>{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const userWithAlbum = await User.findOne({albumNo: req.body.albumNo});
+        const userWithLogin = await User.findOne({login: req.body.login});
+
+        if(userWithLogin || userWithAlbum) {
+            return res
+                .status(409)
+                .send('User with album or login number already exists!');
+        }
+
+        const newUser = new User({
+            name: req.body.name,
+            lastName: req.body.lastName,
+            albumNo: req.body.albumNo,
+            isEnabled: req.body.enabled,
+            login: req.body.login,
+            password: hashedPassword,
+            university: req.body.university
+        });
+        try{
+            const addedUser = await newUser.save();
+            res.status(201);
+            res.json(addedUser);
+        }catch(err){
+            res.status(403);
+            res.json({message:err});
+        }
+    },
+    changePassword: async(req,res) =>{
+        const salt = await bcrypt.genSalt(10);
+        const hashedNewPassword = await bcrypt.hash(req.body.newPassword, salt);
+        const user = await User.findById(req.params.userId);
+
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+
+        if(validPassword && req.body.newPassword === req.body.confirmPassword) {
+            user.update({password: hashedNewPassword}, (err)=>{
+                if(err){
+                    res.status(403).send({message: 'Cannot update password'});
+                } else {
+                    res.status(200).send({message:'Task failed successfully'});
+                }
+            });
+        } else {
+            res.status(403).send({message : 'your account password is not correct or new passwords not matches'});
+        }
+    },
+    test: async(req,res) => {
+        jwt.verify(req.token, 'secretkey', (err, authData) => {
+            if(err){
+                res.sendStatus(403);
+            } else {
+                res.json({
+                    authData
+                });
+            }
+        });
+    }
+}
+
+module.exports = UserController;
--- routes/users.js
+++ routes/users.js
@@ -1,74 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const userController = require('../controller/UsersController')
 
 
-router.get('/findAll', async (req,res)=>{
-    try{
-        const users = await User.find();
-        res.json(users);
-    }catch(err){
-        res.json({message:err})
-    }
-});
+router.post('/test', verifyToken, userController.test);
 
-// router.post('/test', verifyToken, async(req,res) => {
-//     jwt.verify(req.token, 'secretkey', (err, authData) => {
-//         if(err){
-//             res.sendStatus(403);
-//         } else {
-//             res.json({
-//                 authData
-//             });
-//         }
-//     });
-// });
+router.get('/findById/:userId', userController.findByID);
 
-router.get('/findById/:userId', async (req,res)=>{
-   try{
-       const user = await User.findById(req.params.userId);
-       res.json(user)
-   } catch(err){
-       res.json({message:err})
-   }
-});
+router.post('/login', userController.login);
 
-router.post('/login', async (req, res)=>{
-   try{
-       const user = await User.findOne({login : req.body.login});
+router.delete('/delete/:userId', userController.deleteUserByID);
 
-       const validPassword = await bcrypt.compare(
-         req.body.password,
-         user.password
-       );
+router.post('/addUser', userController.addUser);
 
-       if(validPassword){
-           jwt.sign({user},'secretkey',(err,token)=>{
-               res.json({
-                   user: user,
-                   token: token
-               });
-           })
-       }else{
-           throw new Error('Login or password wrong');
-       }
-   } catch (error){
-       res.status(403);
-       res.json({message:error});
-   }
-});
+router.put('/changePassword/:userId', userController.changePassword);
 
-router.delete('/delete/:userId', async(req, res) =>{
-    User.findByIdAndDelete(req.params.userId, (err) => {
-        if (err) {
-            res.status(404).send({message: `User with id: ${req.params.userId} not exists`});
-        } else {
-            res.send({message:`User with id: ${req.params.userId} deleted successfully`});
-        }
-    });
-});
+router.get('/findAll', userController.findAll);
+
+//just put verifyToken inside router to protect route with JWT
 
 function verifyToken(req, res, next){
     const bearerHeader = req.headers['authorization'];
@@ -80,66 +29,5 @@ function verifyToken(req, res, next){
         res.sendStatus(403);
     }
 }
-
-router.post('/addUser', async (req,res)=>{
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const userWithAlbum = await User.findOne({albumNo: req.body.albumNo});
-    const userWithLogin = await User.findOne({login: req.body.login});
-
-    if(userWithLogin || userWithAlbum) {
-        return res
-            .status(409)
-            .send('User with album or login number already exists!');
-    }
-
-    const newUser = new User({
-        name: req.body.name,
-        lastName: req.body.lastName,
-        albumNo: req.body.albumNo,
-        isEnabled: req.body.enabled,
-        login: req.body.login,
-        password: hashedPassword,
-        university: req.body.university
-    });
-   try{
-        const addedUser = await newUser.save();
-        res.status(201);
-        res.json(addedUser);
-   }catch(err){
-       res.status(403);
-       res.json({message:err});
-   }
-});
-
-router.put('/changePassword/:userId', async(req,res) =>{
-    const salt = await bcrypt.genSalt(10);
-    const hashedNewPassword = await bcrypt.hash(req.body.newPassword, salt);
-    const user = await User.findById(req.params.userId);
-
-    const validPassword = await bcrypt.compare(
-        req.body.password,
-        user.password
-    );
-
-    if(validPassword && req.body.newPassword === req.body.confirmPassword) {
-        user.update({password: hashedNewPassword}, (err)=>{
-            if(err){
-                res.status(403).send({message: 'Cannot update password'});
-            } else {
-                res.status(200).send({message:'Task failed successfully'});
-            }
-        });
-    } else {
-        res.status(403).send({message : 'your account password is not correct or new passwords not matches'});
-    }
-    // User.findByIdAndUpdate(req.params.userId,{password: hashedNewPassword}, (err)=>{
-    //     if(err){
-    //         res.status(403).send({message: 'Cannot update password'});
-    //     } else {
-    //         res.status(200).send({message:'Task failed successfully'});
-    //     }
-    // });
-});
 
 module.exports = router;
