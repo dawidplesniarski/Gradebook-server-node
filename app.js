8cfd20d52dd13c9080e99ac63e9b105daa66d00c
dawidplesniarski <dawidplesniarski@gmail.com>
2020-11-01 15:50:13 +0100

    Header added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, authorization'
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
