2b5873a08f459ba3a786ccc01492b51ed61166a9
dawidplesniarski <dawidplesniarski@gmail.com>
2020-11-08 12:24:03 +0100

    EditUniversity added

--- controller/UniversityController.js
+++ controller/UniversityController.js
@@ -28,6 +28,22 @@ const UniversityController = {
         } catch (err) {
             res.json({message: err})
         }
+    },
+    editUniversityData: async (req, res) => {
+        try {
+            const university = await University.findOne({universityName: req.body.universityName});
+            if (!university) {
+                const updatedUniversity = await University.findOneAndUpdate(
+                    {_id: req.body.universityId},
+                    {universityName: req.body.universityName},
+                    {useFindAndModify: false});
+                res.status(200).send(updatedUniversity);
+            } else {
+                res.status(409).send(`University with name ${req.body.universityName} already exists!`);
+            }
+        } catch (err) {
+            res.status(400).send({message: err})
+        }
     }
 }
 
--- routes/university.js
+++ routes/university.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const universityController = require('../controller/UniversityController')
+const verifyToken = require('../utils/verifyToken');
 
 router.get('/findAll', universityController.findAll);
 
-router.post('/addUniversity', universityController.addUniversity);
+router.post('/addUniversity', verifyToken, universityController.addUniversity);
 
 router.get('/findById/:universityId', universityController.findById);
+
+router.put('/editUniversity', verifyToken, universityController.editUniversityData);
 
 module.exports = router;
