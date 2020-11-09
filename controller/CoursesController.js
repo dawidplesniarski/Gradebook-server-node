aa5640649dfee354cc998a1564891d6d5039f0ec
dawidplesniarski <dawidplesniarski@gmail.com>
2020-11-09 18:27:30 +0100

    addCourseWithSubjects and findCourseWithSubjects added

--- controller/CoursesController.js
+++ controller/CoursesController.js
@@ -1,4 +1,5 @@
 const Course = require('../models/Course');
+const CourseSubjects = require('../models/CourseSubjects');
 
 const CoursesController = {
     findAll: async (req, res) => {
@@ -27,6 +28,40 @@ const CoursesController = {
             res.status(200).send(course);
         } catch (err) {
             res.json({message: err})
+        }
+    },
+    addCourseWithSubjects: async (req, res) => {
+        const course = new Course({
+            courseName: req.body.courseName
+        });
+        const courseSubjects = new CourseSubjects({
+            course: req.body.courseName,
+            semesters: req.body.semesters
+        });
+
+        try {
+            const foundCourse = await Course.findOne({courseName: req.body.courseName});
+            if(!foundCourse) {
+                await course.save();
+                await courseSubjects.save();
+                res.status(201).send('Course with subjects created successfully!')
+            } else {
+                res.status(409).send(`Course with name ${req.body.courseName} already exists!`);
+            }
+        } catch(err) {
+            res.status(400).send(err)
+        }
+    },
+    findCourseWithSubjects: async (req, res) => {
+        try {
+            const course = await Course.findOne({courseName: req.params.courseName});
+            const courseSubjects = await CourseSubjects.findOne({course: req.params.courseName});
+            res.status(200).send({
+                course: course,
+                courseSubjects: courseSubjects
+            });
+        } catch (err) {
+            res.status(400).send(err);
         }
     }
 }
--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const coursesController = require('../controller/CoursesController');
+const verifyToken = require('../utils/verifyToken')
 
 router.get('/findAll', coursesController.findAll);
 
 router.post('/addCourse', coursesController.addCourse);
 
 router.get('/findById/:courseId', coursesController.findById);
+
+router.post('/addCourseWithSubjects', verifyToken, coursesController.addCourseWithSubjects);
+
+router.get('/findCourseWithSubjects/:courseName', coursesController.findCourseWithSubjects);
 
 module.exports = router;
